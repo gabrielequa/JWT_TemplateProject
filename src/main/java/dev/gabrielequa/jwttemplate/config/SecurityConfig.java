4032431ac1a904ae6be9598a47b2d29b295cf967
gabrielequa <gabqua2004@gmail.com>
2025-06-03 11:50:08 +0200

    aggiunto swagger-ui

--- src/main/java/dev/gabrielequa/jwttemplate/config/SecurityConfig.java
+++ src/main/java/dev/gabrielequa/jwttemplate/config/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig {
             .requestMatchers("/api/auth/**").permitAll()
             .requestMatchers("/api/test/public").permitAll()
             .requestMatchers("/h2-console/**").permitAll()
+            .requestMatchers("/swagger-ui/**").permitAll()
+            .requestMatchers("/v3/api-docs/**").permitAll()
             .anyRequest().authenticated()
         );
         
